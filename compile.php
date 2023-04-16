6638f322c1311eb6662ecb3be38ddfdb744c1071
yousefaz123 <127549014+yousefaz123@users.noreply.github.com>
2023-04-16 00:44:03 -0400

    Update compile.php

--- compile.php
+++ compile.php
@@ -1,22 +1,43 @@
 <?php
-// Get the language and code from the form
-$language = $_POST['language'];
-$code = $_POST['code'];
 
-// Compile the code
-switch($language) {
-case 'java':
-exec("javac $code");
-break;
-case 'js':
-exec("node $code");
-break;
-case 'python':
-exec("python $code");
-break;
-case 'html':
-echo $code;
-break;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the language and code from the form submission
+    $language = $_POST['language'];
+    $code = $_POST['code'];
+
+    // Compile the code based on the selected language
+    switch ($language) {
+        case 'java':
+            // Compile Java code
+            $output = shell_exec("javac Main.java 2>&1");
+            if (!$output) {
+                $output = shell_exec("java Main 2>&1");
+            }
+            break;
+
+        case 'js':
+            // Compile JavaScript code
+            $output = shell_exec("node Main.js 2>&1");
+            break;
+
+        case 'python':
+            // Compile Python code
+            $output = shell_exec("python Main.py 2>&1");
+            break;
+
+        case 'html':
+            // Compile HTML code
+            $output = shell_exec("open index.html");
+            break;
+
+        default:
+            // Invalid language selection
+            $output = "Invalid language selection";
+            break;
+    }
+
+    // Print the output of the compilation process
+    echo "<pre>$output</pre>";
 }
-?>
 
+?>
