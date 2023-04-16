355f72fa817b2db6f05f28b7c85a53b8076285ee
yousefaz123 <127549014+yousefaz123@users.noreply.github.com>
2023-04-16 00:50:28 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+// get the user's code and language selection from the form
+$userCode = $_POST['code'];
+$language = $_POST['language'];
+
+// determine which command to use based on the selected language
+switch ($language) {
+    case 'java':
+        $command = 'java';
+        $filename = 'Main.java';
+        break;
+    case 'js':
+        $command = 'node';
+        $filename = 'index.js';
+        break;
+    case 'python':
+        $command = 'python';
+        $filename = 'main.py';
+        break;
+    case 'html':
+        // display the HTML code as text
+        echo "<pre>$userCode</pre>";
+        exit();
+        break;
+    default:
+        die('Error: Invalid language selection.');
+}
+
+// create a file with the user's code
+file_put_contents($filename, $userCode);
+
+// execute the command to compile and run the code
+$output = shell_exec("$command $filename");
+
+// display the output
+echo "<pre>$output</pre>";
+
+// remove the file containing the user's code
+unlink($filename);
+?>
